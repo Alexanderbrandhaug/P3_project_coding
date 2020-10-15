0fd78eb664170f103a33a901f935b596f4dccc29
timma01 <timma@stud.ntnu.no>
2020-10-15 11:51:19 +0200

    First

--- JS/blogg_arkiv.js
+++ JS/blogg_arkiv.js
@@ -7,6 +7,7 @@ function make_list(x){
     let node = document.createElement("li");
     let button = document.createElement("button")
     button.innerText = x;
+    button.id = x;
     button.type = "button";
     button.onclick = function() {create_sub_list(this)};
 
@@ -25,18 +26,48 @@ function create_sub_list(x) {
         for (let y = 0; y < months.length; y++){
             let child = document.createElement("li");
             child.innerText = months[y];
+            child.name = months[y];
             childList.appendChild(child);
         }
 
         x.appendChild(childList);
 
-        x.name = "extended"
+        x.name = "extended";
+        addHeading();
     }
     else {
-        x.removeChild(x.lastChild)
-        x.name = "closed"
+        x.removeChild(x.lastChild);
+        x.name = "closed";
+    }
+    /*console.log("Test")*/
+}
+
+const parentDate = document.getElementsByClassName("blogg_content")
+
+function addHeading() {
+
+    let dates = []
+
+    for (let x = 0; x < parentDate.length; x++) {
+        parentDate[x].id = parentDate[x].querySelector("h3").innerText;
+        dates.push(parentDate[x].querySelector("h3").innerText.split("."));
     }
 
+    for (let x = 0; x < dates.length; x++) {
+        for (let y = 0; y < dates[x].length; y++){
+            dates[x][y] = parseInt(dates[x][y]);
+        }
+    }
 
-    /*console.log("Test")*/
+    for (let x = 0; x < dates.length; x++) {
+        let newList = document.createElement("ul")
+        let node = document.createElement("li");
+        node.href = dates[x][0] + "." + dates[x][1] + "." + dates[x][2];
+        node.innerText = parentDate[x].querySelector("h1").innerText;
+        newList.appendChild(node);
+        document.getElementById(dates[x][2]).parentNode.querySelector("ul").children.item(dates[x][1] - 1).appendChild(newList);
+
+    }
+
+    console.log(dates)
 }
