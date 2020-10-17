154ad66072a84e1b4a547a26b449c628236b397a
sofie-rk <sofierk@stud.ntnu.no>
2020-10-17 12:53:20 +0200

    changes 17/10

--- JS/nettbutikk.js
+++ JS/nettbutikk.js
@@ -1,6 +1,89 @@
 // Get list element from nettbutikk.html
 
+const left = document.getElementById("left")
 const right = document.getElementById("right")
+
+
+const articles = [
+    {id: 1, name: "Artikkel 1", price: "849 kr", description: "Beskrivelse her", image_loc: "images/strikkp1.jpg", sizes: ["0-3 måneder", "6-12 måneder", "1-2 år"]},
+    {id: 2, name: "Artikkel 2", price: "849 kr",description: "Beskrivelse her",image_loc: "images/strikkp1.jpg", sizes: ["6-12 måneder", "1-2 år", "3-4 år"]},
+
+]
+
+
+
+for (let i = 0; i < articles.length; i++) {
+    // Create division where information about the article will be displayed
+    var article_division = document.createElement("div")
+    article_division.setAttribute('class', 'article')
+
+    // Article name information
+    var articleName = document.createElement('p')
+    articleName.setAttribute('class', 'article-name')
+    articleName.appendChild(document.createTextNode(articles[i].name))
+
+    // Article price information
+    var articlePrice = document.createElement('p')
+    articlePrice.setAttribute('class', 'article-price')
+    articlePrice.appendChild(document.createTextNode(articles[i].price))
+
+    // Article description
+    var articleDescription = document.createElement('p')
+    articleDescription.setAttribute('class', 'article-description')
+    articleDescription.appendChild(document.createTextNode(articles[i].description))
+
+    // Article image
+    var articleImage = document.createElement('img')
+    articleImage.setAttribute('src', articles[i].image_loc)
+
+    // Create division for size selection dropdown
+    // var sizeDropDown = document.createElement('div')
+    // sizeDropDown.setAttribute('div', 'sizeDropDown')
+    // sizeDropDown.setAttribute('id', 'sizeDD'+articles[i].id)
+
+    // Button for size selection, will display dropdown menu for sizes
+    var sizeBtn = document.createElement('button')
+    sizeBtn.setAttribute('class', 'sizeBtn')
+    sizeBtn.innerHTML = "Velg størrelse"
+    // When pressed, the dropdown menu will be displayed 
+    sizeBtn.addEventListener('click', function() {updateBtn("options_"+articles[i].id)})
+
+    // Divisions for the possible size choickes
+    var options = document.createElement("div")
+    options.setAttribute('class', 'sizeOptions')
+    options.setAttribute('id', 'options_'+articles[i].id)
+
+    // Iterate through all size options for the article, and make the size available in 
+    // dropdown menu
+    for (let j = 0; j < articles[i].sizes.length; j++){
+        var option = document.createElement('a')
+        option.addEventListener('click', updateCheckout)
+        option.innerHTML = articles[i].sizes[j]
+        options.appendChild(option)
+    }
+
+
+    //sizeDropDown.appendChild(sizeBtn)
+    //sizeDropDown.appendChild(options)
+
+
+    article_division.appendChild(articleName)
+    article_division.appendChild(articlePrice)
+    article_division.appendChild(articleDescription)
+    article_division.appendChild(articleImage)
+    
+    //article_division.appendChild(sizeDropDown)
+
+    article_division.appendChild(sizeBtn)
+    article_division.appendChild(options)
+
+    left.appendChild(article_division)
+}
+
+
+function updateCheckout(){
+    console.log("Handlekurv oppdateres")
+}
 
 function displayDropdown(dropdown_id) {
     var dropdown = document.getElementById(dropdown_id)
@@ -8,12 +91,19 @@ function displayDropdown(dropdown_id) {
 }
 
 function selectSize(size) {
-    
     var line = document.createElement("P")
     var textNode = document.createTextNode(size)
 
     line.appendChild(textNode)
 
     right.appendChild(line)
+
+}
+
+function updateBtn(article_id) {
+    console.log("Button pressed")
+    console.log(article_id)
+
+    document.getElementById(article_id).style.display = "block"
 
 }
