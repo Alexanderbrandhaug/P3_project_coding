ef1952e5cc3904f8ec14cbd8e760a1b6eb0157a4
timma01 <timma@stud.ntnu.no>
2020-10-26 15:05:06 +0100

    Update galleri.js

--- JS/galleri.js
+++ JS/galleri.js
@@ -7,41 +7,49 @@ function main(bilde) {
         document.getElementById("right").style.display = "block";
         document.getElementById("right").style.visibility = "visible";
         document.getElementById("button_div").style.visibility = "visible";
-    }
 
-
-    console.log('test3')
-    if (parent.querySelector("img")!= null) {
-        if (bilde.src === parent.querySelector("img").src) {
-            while (parent.children.length > 0) {
-                parent.removeChild(parent.lastChild)
-            }
-            let node = document.createElement("h1");
-            node.innerText = "Velkommen til galleriet!";
-            let node2 = document.createElement("p");
-            node.innerText = "Velkommen til galleriet!"
-            node2.innerText = "Trykk på et bilde for å se det større. Trykk på samme bilde igjen for å ta det bort."
-            parent.appendChild(node);
-            parent.appendChild(node2)
-            console.log('test1')
-            return;
-        }
-
-    }
-    if (parent.children.length !== 0) {
-        while (parent.children.length > 0) {
-            parent.removeChild(parent.lastChild)
-        }
         let node = document.createElement("img");
         node.src = bilde.src;
         parent.appendChild(node);
         node.id = "current_picture"
         console.log('test2')
+    } else {
+
+        if (parent.querySelector("img")!= null) {
+        if (bilde.src === parent.querySelector("img").src) {
+            while (parent.children.length > 0) {
+                parent.removeChild(parent.lastChild)
+            }
+                let node = document.createElement("h1");
+                node.innerText = "Velkommen til galleriet!";
+                let node2 = document.createElement("p");
+                node.innerText = "Velkommen til galleriet!"
+                node2.innerText = "Trykk på et bilde for å se det større. Trykk på samme bilde igjen for å ta det bort."
+                parent.appendChild(node);
+                parent.appendChild(node2)
+                return;
+            }
+
+        }
+        if (parent.children.length !== 0) {
+            while (parent.children.length > 0) {
+                parent.removeChild(parent.lastChild)
+            }
+            let node = document.createElement("img");
+            node.src = bilde.src;
+            parent.appendChild(node);
+            node.id = "current_picture"
+        }
+
     }
+
+
+
 }
 
 function closeFunction() {
     document.getElementById("right").style.display = "none";
+    document.getElementById("right").innerHTML = "";
     document.getElementById("button_div").style.visibility = "hidden";
     console.log("knapp");
 }
