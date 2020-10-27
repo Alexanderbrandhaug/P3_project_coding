f0db2b4323de86256852a2eb986431e536bab691
idamatre <ida.matre@gmail.com>
2020-10-27 12:40:55 +0100

    .

--- JS/galleri.js
+++ JS/galleri.js
@@ -1,9 +1,8 @@
 let parent = document.getElementById("right");
 
 function main(bilde) {
-
+    //For smaller screens (modularity)
     if (window.innerWidth <= 800){
-        console.log("Test5")
         document.getElementById("right").style.display = "block";
         document.getElementById("right").style.visibility = "visible";
         document.getElementById("button_div").style.visibility = "visible";
@@ -12,14 +11,15 @@ function main(bilde) {
         node.src = bilde.src;
         parent.appendChild(node);
         node.id = "current_picture"
-        console.log('test2')
     } else {
-
+        //If there is no picture on display
         if (parent.querySelector("img")!= null) {
-        if (bilde.src === parent.querySelector("img").src) {
-            while (parent.children.length > 0) {
-                parent.removeChild(parent.lastChild)
+            //If the user clicks on the same image twice it will be removed
+            if (bilde.src === parent.querySelector("img").src) {
+                while (parent.children.length > 0) {
+                    parent.removeChild(parent.lastChild)
             }
+            //After removing everything in the div it will make sure to put the text back
             let node = document.createElement("h1");
             node.innerText = "Velkommen til galleriet!";
             let node2 = document.createElement("p");
@@ -33,10 +33,12 @@ function main(bilde) {
             }
 
         }
+        //If there is something in the right div it will be removed
         if (parent.children.length !== 0) {
             while (parent.children.length > 0) {
                 parent.removeChild(parent.lastChild)
             }
+            //Uses the src of the photo to display the same photo as clicked
             let node = document.createElement("img");
             node.src = bilde.src;
             parent.appendChild(node);
@@ -48,10 +50,9 @@ function main(bilde) {
 
 
 }
-
+//Function for button. Removes eveything inside right div
 function closeFunction() {
     document.getElementById("right").style.display = "none";
     document.getElementById("right").innerHTML = "";
     document.getElementById("button_div").style.visibility = "hidden";
-    console.log("knapp");
 }
